d3f1ad2dfe8f39b91b843e357ee623bd12ed1de0
Thomas Allmer <d4kmor@gmail.com>
2022-04-28 13:06:19 +0200

    WIP: mixin support for static properties

--- packages/analyzer/src/features/framework-plugins/lit/static-properties.js
+++ packages/analyzer/src/features/framework-plugins/lit/static-properties.js
@@ -3,6 +3,9 @@ import { getDefaultValuesFromConstructorVisitor } from '../../analyse-phase/crea
 import { handleJsDoc } from '../../analyse-phase/creators/handlers.js';
 import { isAlsoAttribute, hasStaticKeyword, getPropertiesObject, getAttributeName, reflects } from './utils.js';
 
+import { extractMixinNodes, isMixin } from '../../../utils/mixins.js';
+import { handleName } from '../../analyse-phase/creators/createMixin.js';
+
 /**
  * STATIC-PROPERTIES
  * 
@@ -13,68 +16,100 @@ export function staticPropertiesPlugin() {
     name: 'CORE - LIT-STATIC-PROPERTIES',
     analyzePhase({ts, node, moduleDoc, context}){
       switch (node.kind) {
-        case ts.SyntaxKind.ClassDeclaration:    
-          const className = node?.name?.getText();
-          const currClass = moduleDoc?.declarations?.find(declaration => declaration.name === className);
-    
-          node?.members?.forEach(member => {
-            if (hasStaticKeyword(member) && member.name.text === 'properties') {
-              const propertiesObject = getPropertiesObject(member);
-
-              propertiesObject?.properties?.forEach(property => {
-
-                let classMember = {
-                  kind: 'field',
-                  name: property?.name?.getText() || '',
-                  privacy: 'public',
-                };
-
-                classMember = handleJsDoc(classMember, property);
-
-                if (isAlsoAttribute(property)) {
-                  const attribute = createAttributeFromField(classMember);
-
-                  /**
-                   * If an attribute name is provided
-                   * @example @property({attribute:'my-foo'})
-                   */
-                  const attributeName = getAttributeName(property);
-                  if(attributeName) {
-                    attribute.name = attributeName;
-                    classMember.attribute = attributeName;
-                  } else {
-                    classMember.attribute = classMember.name;
-                  }
-
-                  if(reflects(property)) {
-                    classMember.attribute = attribute.name;
-                    classMember.reflects = true;
-                  }
-
-                  const existingAttr = currClass?.attributes?.find(attr => attr.name === attribute.name);
-                  if(!existingAttr) {
-                    currClass.attributes.push(attribute);
-                  } else {
-                    currClass.attributes = currClass?.attributes?.map(attr => attr.name === attribute.name ? ({...attr, ...attribute}) : attr);
-                  }
-                }
-
-                const existingField = currClass?.members?.find(field => field.name === classMember.name);
-
-                if(!existingField) {
-                  currClass.members.push(classMember);
-                } else {
-                  currClass.members = currClass?.members?.map(field => field.name === classMember.name ? ({...field, ...classMember}) : field);
-                }
-              });
-              return;
-            }
-          });
-
-          /** Get default values */
-          getDefaultValuesFromConstructorVisitor(node, currClass, context);
+        case ts.SyntaxKind.VariableStatement:
+        case ts.SyntaxKind.FunctionDeclaration:
+          if(isMixin(node)) {
+            const { mixinFunction, mixinClass } = extractMixinNodes(node);
+            const { name } = handleName({}, mixinFunction);
+            handleStaticProperties(mixinClass, moduleDoc, context, name);
+          }
           break;
-        }
+
+        case ts.SyntaxKind.ClassDeclaration:
+          handleStaticProperties(node, moduleDoc, context);
+          break;
       }
+      // if (node.members) {
+      //   console.log('---------- members --------');
+      //   console.log(node.members.map(member => member && member.name ? member.name.escapedText : member.body.statements[0].expression.expression));
+      // }
+    }
   }
+}
+
+function handleStaticProperties(classNode, moduleDoc, context, mixinName = null) {
+  let className;
+  if(!mixinName) {
+    className = classNode?.name?.getText();
+  } else {
+    className = mixinName;
+  }
+  const currClass = moduleDoc?.declarations?.find(declaration => declaration.name === className);  
+
+  console.log({ className, mixinName, currClass });
+
+  classNode?.members?.forEach(member => {
+    if (hasStaticKeyword(member) && member.name.text === 'properties') {
+      const propertiesObject = getPropertiesObject(member);
+
+      propertiesObject?.properties?.forEach(property => {
+        let classMember = {
+          kind: 'field',
+          name: property?.name?.getText() || '',
+          privacy: 'public',
+        };
+
+        
+
+        classMember = handleJsDoc(classMember, property);
+
+        if (isAlsoAttribute(property)) {
+          const attribute = createAttributeFromField(classMember);
+
+          /**
+           * If an attribute name is provided
+           * @example @property({attribute:'my-foo'})
+           */
+          const attributeName = getAttributeName(property);
+          if(attributeName) {
+            attribute.name = attributeName;
+            classMember.attribute = attributeName;
+          } else {
+            classMember.attribute = classMember.name;
+          }
+
+          if(reflects(property)) {
+            classMember.attribute = attribute.name;
+            classMember.reflects = true;
+          }
+
+          const existingAttr = currClass?.attributes?.find(attr => attr.name === attribute.name);
+          if(!existingAttr) {
+            currClass.attributes.push(attribute);
+          } else {
+            currClass.attributes = currClass?.attributes?.map(attr => attr.name === attribute.name ? ({...attr, ...attribute}) : attr);
+          }
+        }
+
+        const existingIndex = currClass?.members?.findIndex(field => field.name === classMember.name);
+
+        // console.log('existingField', currClass.members, existingIndex);
+        if(!existingIndex) {
+          currClass.members.push(classMember);
+        } else {
+          // console.log('existingField', currClass.members[existingIndex]);
+          // console.log({ classMember });
+
+          // currClass.members = currClass?.members?.map(field => field.name === classMember.name ? ({...field, ...classMember}) : field);
+
+        }
+      });
+      return;
+    }
+  });
+
+  // console.log('currClass', currClass.members);
+
+  /** Get default values */
+  getDefaultValuesFromConstructorVisitor(classNode, currClass, context);  
 }
